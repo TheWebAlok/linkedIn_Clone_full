eb6adfdf70323a76ffb1ce14f015862fea8041e7
alokprakash7766 <your_email@domain.com>
2026-02-17 21:55:47 +0530

    API

--- frontend/src/config/index.jsx
+++ frontend/src/config/index.jsx
@@ -2,7 +2,7 @@ const { default: axios } = require("axios");
 
 
 
-export const BASE_URL = "https://linkedin-clone-full.onrender.com/"
+export const BASE_URL = "https://linkedin-clone-full.onrender.com"
  export const clientServer = axios.create({
       baseURL: BASE_URL,
 })
